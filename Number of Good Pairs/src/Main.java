7e67ba17d29db44275735dcec3017652972c098d
antondubov <antondubov1@yandex.ru>
2023-06-01 18:02:30 +0300

    2 for cycles

---
+++ Number of Good Pairs/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution demo = new Solution();
+        System.out.println(demo.numIdenticalPairs(new int[]{1,2,3,1,1,3}));
+    }
+}
