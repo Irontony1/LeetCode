c5b0913bd5d31888aa678e8ba1a9c9f6ad3c901a
antondubov <antondubov1@yandex.ru>
2023-06-13 09:59:04 +0300

    For in for

---
+++ Richest Customer Wealth/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution demo = new Solution();
+        System.out.println(demo.maximumWealth(new int[][]{{1,5},{7,3},{3,5}}));
+    }
+}
